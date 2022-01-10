2ca02ac4b6feae6b3301d45edad7e3b1e16c8f86
Tifanny Rivera <trivera51580@gmail.com>
2022-01-09 18:33:55 -0700

    added social media app to the list of projects

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -7,6 +7,14 @@ export default function Works() {
   const data = [
     {
       id: "1",
+      title: "Social Media App",
+      desc: "This is a mock social media application where user can login, create, post, and delete content.",
+      img: "./assets/newpost.png",
+      link: "https://tr-socialmediaapp.herokuapp.com/",
+      github: "https://github.com/trivera777/SocialMediaApp"
+    },
+    {
+      id: "2",
       title: "Scrypts of Betrayal",
       desc: "This is a javascript fueled web based video game. The user is able to login, and play a basic 2d pixel art top-down video game. I took over the front end side of this application and am very pleased with the results! ",
       img: "./assets/scrypts.png",
@@ -15,7 +23,7 @@ export default function Works() {
       list: ""
     },
     {
-      id: "2",
+      id: "3",
       title: "National Parks Day Trip Planner",
       desc: "Our project allows a user to search different National Parks in a specific state they want to travel to. I enjoyed developing the front end of this project.",
       img: "./assets/np.png",
@@ -23,15 +31,15 @@ export default function Works() {
       github: "https://github.com/trivera777/National-Parks-Day-Trip-Planner"
     },
     {
-      id: "3",
+      id: "4",
       title: "Furever Home",
-      desc: "Use this app to find a Furever friend near you! Just enter in what kind of pet you are looking for, and what area and let the magic happen. All of the pets will be populated so you can give them tier FurEver Home! The app calls the petfinder API and returns pets near you that are ready for their FurEver Home!",
+      desc: "Use this app to find a Furever friend near you! This app calls the petfinder API and returns pets near you that are ready for their FurEver Home!",
       img: "./assets/fureverhome.png",
       link: "FurEverHome.info",
       github: "https://github.com/trivera777/Furever-Home"
     },
     {
-      id: "4",
+      id: "5",
       title: "Rad Media",
       desc: "This is a simple and fun site that I created for a friend. I wanted to showcase his awesome photography and show off a different style of design that I have never done before. I enjoyed adding the distorted title on the site.",
       img: "./assets/radmedia.png",
@@ -39,7 +47,7 @@ export default function Works() {
       github: "https://github.com/trivera777/RadMedia"
     },
     {
-      id: "5",
+      id: "6",
       title: "Tech Blog",
       desc: "This app is a tech blog that allows users to create a profile to be able to create, edit and delete their own blog posts. User is also able to comment on other users posts.",
       img: "./assets/techblog.png",
@@ -47,13 +55,13 @@ export default function Works() {
       github: "https://github.com/trivera777/TechBlog"
     },
     {
-      id: "6",
+      id: "7",
       title: "Note Taker",
       desc: "An application to create and store notes with Express.js and retrieve note data from JSON file.",
       img: "./assets/notetaker.png",
       link: "https://tr-notetaker.herokuapp.com/",
       github: "hhttps://github.com/trivera777/NoteTaker"
-    },
+    }
   ];
 
   const handleClick = (way) => {
