728b3ab2fc34736c828cf0fdd69ecd5051294912
Tifanny Rivera <trivera51580@gmail.com>
2022-01-28 16:20:43 -0700

    moving badge link and certificate link to about me section instead of putting it in the navbar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,20 +7,40 @@ export default function About() {
       <div className="wrapper">
         <div className="title">
           <h1>about.</h1>
-          </div>
-          <div className="aboutContainer">
-            Hi my name is Tifanny! I come from a small town in Colorado called Greeley.
-            I was born in Mexico but raised in Colorado the majority of my life.
-            <br />
-            For the past five years I have been working in the banking industry. 
-            Five years is a long time. For me anyway, and for the goals that I have 
-            for my life. 
-            <br />
-            By some miracle, as I was hunting for jobs, I came across
-            a jo that required coding experience. I wasn't really sure what that was,
-            and that's what started it all. 
+        </div>
+        <div className="aboutContainer">
+          Hi my name is Tifanny! I come from a small town in Colorado called
+          Greeley. I was born in Mexico but raised in Colorado the majority of
+          my life.
+          <br />
+          For the past five years I have been working in the banking industry.
+          Five years is a long time. For me anyway.
+          <br />
+          One day as I was browsing through job postings, I came across a job
+          that required coding experience. I wasn't really sure what that was,
+          looked into coding, and that's what started it all.
+          <br />I immediately enrolled myself in a Coding Bootcamp that was
+          offered at the University of Denver. Six months later and about 500
+          hours plus of bootcamp structure, I received a
+          {" "}
+          <a
+            href="https://www.credly.com/badges/5baf8c6e-b123-4aa6-93b4-630af8b31a97/linked_in"
+            target="_blank"
+          >
+          verifed badge 
+          </a>
+          {" "}
+           and a 
+           {" "}
+           <a
+           href="https://drive.google.com/file/d/1BGVKC7ko9hxC-JgvzyfjBU3LOKJkH1ZM/view?usp=sharing"
+           target="_blank"
 
-          </div>
+           >
+           certificate of competion.
+           </a>
+           {" "}
+        </div>
       </div>
     </div>
   );
