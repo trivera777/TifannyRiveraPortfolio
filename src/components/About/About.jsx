c4a9502e87d8b2768d61add5b376eace42b84d67
Tifanny Rivera <trivera51580@gmail.com>
2022-01-29 23:32:31 -0700

    redid my about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,39 +8,35 @@ export default function About() {
         <div className="title">
           <h1>about.</h1>
         </div>
-        <div className="aboutContainer">
-          <img src='./assets/me.png' style={{ height: '35vh'}} />
-          Hi my name is Tifanny! I come from a small town in Colorado called
-          Greeley. I was born in Mexico but raised in Colorado the majority of
-          my life.
-          <br />
-          For the past five years I have been working in the banking industry.
-          Five years is a long time. The longer I stayed there, the more I 
-          <br />
-          One day as I was browsing through job postings, I came across a job
-          that required coding experience. I wasn't really sure what that was,
-          looked into coding, and that's what started it all.
-          <br />I immediately enrolled myself in a Coding Bootcamp that was
-          offered at the University of Denver. Six months later and about 500
-          hours plus of bootcamp structure, I received a
-          {" "}
-          <a
-            href="https://www.credly.com/badges/5baf8c6e-b123-4aa6-93b4-630af8b31a97/linked_in"
-            target="_blank"
-          >
-          verifed badge 
-          </a>
-          {" "}
-           and a 
-           {" "}
-           <a
-           href="https://drive.google.com/file/d/1BGVKC7ko9hxC-JgvzyfjBU3LOKJkH1ZM/view?usp=sharing"
-           target="_blank"
-
-           >
-           certificate of competion.
-           </a>
-           {" "}
+        <div className="aboutContainer" style={{ color: "black" }}>
+          <img src="./assets/me.png" style={{ height: "35vh" }} />
+          <div className="text">
+            Hi my name is Tifanny! I live in Colorado and I have lived here for
+            the majority of my life. I am currently a personal banker offering
+            five years of diversified experience with excellent communication
+            and problem-solving skills. I am able to focus on projects, develop
+            strategies and meet or exceed company expectations.
+            <br />
+            I am a recent Coding Bootcamp graduate with fundamental
+            knowledge of software design, development, and testing. Seeking to
+            utilize broad educational background with excelent analytical,
+            technical, and programming skills to thrive as an entry-level
+            software engineer. <br />
+            View my{" "}
+            <a
+              href="https://www.credly.com/badges/5baf8c6e-b123-4aa6-93b4-630af8b31a97/linked_in"
+              target="_blank"
+            >
+              verifed badge
+            </a>{" "}
+            and my{" "}
+            <a
+              href="https://drive.google.com/file/d/1BGVKC7ko9hxC-JgvzyfjBU3LOKJkH1ZM/view?usp=sharing"
+              target="_blank"
+            >
+              certificate of competion.
+            </a>{" "}
+          </div>
         </div>
       </div>
     </div>
