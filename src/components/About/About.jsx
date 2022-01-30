cc4c7e7ae3c6fdb63d08eaca76fd02e802c05e25
Tifanny Rivera <trivera51580@gmail.com>
2022-01-30 12:59:59 -0700

    working about me, specifically links

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,8 +9,8 @@ export default function About() {
           <h1>about.</h1>
         </div>
         <div className="aboutContainer" style={{ color: "black" }}>
-          <img src="./assets/me.png" style={{ height: "35vh" }} />
           <div className="text">
+          <img src="./assets/me.png" style={{ height: "35vh" }} />
             Hi my name is Tifanny! I live in Colorado and I have lived here for
             the majority of my life. I am currently a personal banker offering
             five years of diversified experience with excellent communication
@@ -23,19 +23,21 @@ export default function About() {
             technical, and programming skills to thrive as an entry-level
             software engineer. <br />
             View my{" "}
-            <a
+            <a className="link"
               href="https://www.credly.com/badges/5baf8c6e-b123-4aa6-93b4-630af8b31a97/linked_in"
               target="_blank"
             >
               verifed badge
-            </a>{" "}
-            and my{" "}
-            <a
+            </a>
+            {" "}
+
+            and my
+            {/* <a
               href="https://drive.google.com/file/d/1BGVKC7ko9hxC-JgvzyfjBU3LOKJkH1ZM/view?usp=sharing"
               target="_blank"
             >
               certificate of competion
-            </a>{" "}
+            </a> */}
             from the University of Denver.
           </div>
         </div>
