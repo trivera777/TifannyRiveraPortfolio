150521bc93daf2f6e0a9f5f4d0076aaec93b362d
Tifanny Rivera <trivera51580@gmail.com>
2022-02-16 20:26:35 -0700

    testing footer mobile view:

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ function App() {
                 <Works />
                 <Clients />
                 {/* <Contact /> */}
-                <Footer />
             </div>
+                <Footer />
         </div>
     )
 }
