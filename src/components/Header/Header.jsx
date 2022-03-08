079d89270346510e5cf3c806cbff0ba220b615e9
Tifanny Rivera <trivera51580@gmail.com>
2022-03-07 20:12:29 -0700

    adding header components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import "./header.scss";
 
 export default function Header() {
-    return (
-      <div> header
+  return (
+    <div className="header">
+      <div className="logo">
+        tifanny rivera
+        <div className="menu">
+          <div className="webMenu">web</div>
+          <div className="mobileMenu">mobile</div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
