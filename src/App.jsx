7b3166847c44615dc715ba46f3cc1c2f04b0d87a
Tifanny Rivera <trivera51580@gmail.com>
2022-03-11 17:47:22 -0700

    setting theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import Home from "./components/Home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.scss";
 
+import React from "react";
+import Home from "./components/Home/Home";
+import GlobalStyle from "./globalStyles";
+
 function App() {
   return (
-    <div className="app">
+    <>
+    <GlobalStyle />
         <Home />
-    </div>
+    </>
   );
 }
 
---
+++ src/themes.jsx
@@ -0,0 +1,7 @@
+export const lightTheme = {
+    body:"#FCF6F4",
+    text:"000000",
+    fontFamily:"'Source Sans Pro', sans-serif",
+    bodyRgba:"252,246,244",
+    textRgba:"0,0,0"
+}
