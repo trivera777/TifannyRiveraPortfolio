d2f02706357f06ca07be62fa6c670468d7850d1f
Tifanny Rivera <trivera51580@gmail.com>
2022-03-11 18:11:22 -0700

    updated correct components

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,27 @@ import "./app.scss";
 
 import React from "react";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
 import GlobalStyle from "./globalStyles";
 import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/themes";
+import { Route, Switch } from "react-router";
 
 function App() {
   return (
     <>
     <GlobalStyle />
-    <ThemeProvider>
-      
+    <ThemeProvider theme={lightTheme}>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={About} />
+      <Route exact path="/" component={Skills} />
+      <Route exact path="/" component={Work} />
+      <Route exact path="/" component={Contact} />
+    </Switch>
     </ThemeProvider>
         <Home />
     </>
