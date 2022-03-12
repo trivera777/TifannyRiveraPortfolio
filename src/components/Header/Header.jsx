4e3d657c534b1ec3dd33607e7f20be12c57408d1
Tifanny Rivera <trivera51580@gmail.com>
2022-03-11 17:27:36 -0700

    installing styled-components library

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,31 +4,7 @@ import "./header.scss";
 export default function Header() {
   return (
     <>
-      <header className="header">
-        <div className="container d_flex">
-          <div className="logo">header</div>
-        </div>
-        <div className="navLink">
-          <ul className="link f_flex uppercase">
-            <li>
-              <a href="home">home</a>
-            </li>
-            <li>
-              <a href="portfolio">portfolio</a>
-            </li>
-            <li>
-              <a href="resume">resume</a>
-            </li>
-            <li>
-              <a href="clients">clients</a>
-            </li>
-            <li>
-              <a href="contact">contact</a>
-            </li>
-          </ul>
-        </div>
-      </header>
-          <button className="toggle">toggle</button>
+     
     </>
   );
 }
