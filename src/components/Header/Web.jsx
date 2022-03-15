4ec7350c34efee16ab8efb08de9f93a92c6651c7
Tifanny Rivera <trivera51580@gmail.com>
2022-03-14 22:05:01 -0600

    main menu hides when in mobile view

--- src/components/Header/Web.jsx
+++ src/components/Header/Web.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import './web.scss'
+import "./web.scss";
 
 export default function Web() {
   return (
     <div className="web" id="web">
       <div className="webOption">
         <a href="#projects">
-        <i className="fi fi-rr-edit-alt optionIcon"> </i> Projects
+          <i className="fi fi-rr-edit-alt optionIcon"></i>Projects
         </a>
       </div>
       <div className="webOption">
         <a href="#skills">
-        <i className="fi fi-rr-laptop optionIcon"></i>Skills
+          <i className="fi fi-rr-laptop optionIcon"></i>Skills
         </a>
       </div>
       <div className="webOption">
         <a href="#work">
-        <i className="fi fi-rr-briefcase optionIcon"></i>Work
+          <i className="fi fi-rr-briefcase optionIcon"></i>Work
         </a>
       </div>
       <div className="webOption">
         <a href="#contact">
-        <i className="fi fi-rr-user optionIcon"></i>Contact
+          <i className="fi fi-rr-user optionIcon"></i>Contact
         </a>
       </div>
     </div>
