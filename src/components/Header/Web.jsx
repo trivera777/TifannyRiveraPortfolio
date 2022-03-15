8f50b6647bc0a687a91165f3068b5734d7ca6b41
Tifanny Rivera <trivera51580@gmail.com>
2022-03-14 21:28:06 -0600

    styling menu links

--- src/components/Header/Web.jsx
+++ src/components/Header/Web.jsx
@@ -1,25 +1,26 @@
 import React from "react";
+import './web.scss'
 
 export default function Web() {
   return (
     <div className="web" id="web">
       <div className="webOption">
-        <a href="project">
+        <a href="#projects">
         <i className="fi fi-rr-edit-alt optionIcon"> </i> Projects
         </a>
       </div>
       <div className="webOption">
-        <a href="project">
+        <a href="#skills">
         <i className="fi fi-rr-laptop optionIcon"></i>Skills
         </a>
       </div>
       <div className="webOption">
-        <a href="project">
+        <a href="#work">
         <i className="fi fi-rr-briefcase optionIcon"></i>Work
         </a>
       </div>
       <div className="webOption">
-        <a href="project">
+        <a href="#contact">
         <i className="fi fi-rr-user optionIcon"></i>Contact
         </a>
       </div>
