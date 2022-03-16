460d16ae631912905fa5b531efc0f6c617bdafaf
Tifanny Rivera <trivera51580@gmail.com>
2022-03-15 20:34:03 -0600

    working side menu for mobile view

--- src/components/Header/Mobile.jsx
+++ src/components/Header/Mobile.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import "./mobile.scss";
 
-export default function Mobile() {
+export default function Mobile({ isOpen, setIsOpen }) {
   return (
     <div className="mobile" id="mobile">
-     mobile
+      <div className="closeIcon" onClick={() => setIsOpen(!isOpen)} >
+        <i className="fi fi-rr-cross"></i>
+      </div>
+      <div className="mobileOptions">
+      <div className="mobileOption">
+        <a href="#projects">
+          <i className="fi fi-rr-edit-alt optionIcon"></i>Projects
+        </a>
+      </div>
+      <div className="mobileOption">
+        <a href="#skills">
+          <i className="fi fi-rr-laptop optionIcon"></i>Skills
+        </a>
+      </div>
+      <div className="mobileOption">
+        <a href="#work">
+          <i className="fi fi-rr-briefcase optionIcon"></i>Work
+        </a>
+      </div>
+      <div className="mobileOption">
+        <a href="#contact">
+          <i className="fi fi-rr-user optionIcon"></i>Contact
+        </a>
+      </div>
+      </div>
     </div>
   );
 }
