b048ea9ce1d99b1339d4db175088765193b5a044
Tifanny Rivera <trivera51580@gmail.com>
2022-03-15 22:21:12 -0600

    added about me pic

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import './about.scss'
+import React from "react";
+import "./about.scss";
 
-export default function About(){
-    return (
-        <div className="about" id="about">
-            <div className="aboutTop">
-                <div className="aboutInfo">
-                    Hello there, I am Tifanny Rivera, a front-end developer. 
-                </div>
-                <div className="aboutPhoto"></div>
-            </div>
-            <div className="aboutBottom">
-
-            </div>
+export default function About() {
+  return (
+    <div className="about" id="about">
+      <div className="aboutTop">
+        <div className="aboutInfo">
+          Hello there, I am
+          <br />
+          <span className="infoName">Tifanny Rivera</span>
+          <br />
+          front-end developer.
         </div>
-    )
+        <div className="aboutPhoto">
+          <img
+            src="../assets/Hand coding-rafiki.png"
+            className="picture"
+            alt="gif"
+          />{" "}
+        </div>
+      </div>
+      <div className="aboutBottom"></div>
+    </div>
+  );
 }
