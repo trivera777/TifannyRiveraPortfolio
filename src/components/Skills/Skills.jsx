f362f48943852b1b33b059ecafd61b521c122c13
Tifanny Rivera <trivera51580@gmail.com>
2022-03-17 11:39:17 -0600

    created skills data file

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './skills.scss'
+import Separator from "../Separator/Separator";
+
 
 export default function Skills(){
     return (
         <div className="skills" id="skills">
+            <Separator />
             skills
+            <i className="devicon-react-original colored"></i>
+
         </div>
     )
 }
---
+++ src/components/Skills/skillsData.jsx
@@ -0,0 +1,19 @@
+export const SkillsData =[
+    {
+        type: 'Frontend',
+        list: [
+            {
+                name:'ReactJS',
+                icon: <i className="devicon-react-original colored"></i>,
+            },
+            {
+                name:'ReactJS',
+                icon: <i className="devicon-react-original colored"></i>,
+            },
+            {
+                name:'ReactJS',
+                icon: <i className="devicon-react-original colored"></i>,
+            }
+        ]
+    }
+]
