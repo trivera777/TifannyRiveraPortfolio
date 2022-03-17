68939f6e3affd5ddf8c30c32f165df6c631fd7a8
Tifanny Rivera <trivera51580@gmail.com>
2022-03-16 19:42:38 -0600

    created separator

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { ProjectData } from './Data/ProjectData'
-import  ProjectCard from './Project Cards/ProjectCard'
+import { ProjectData } from "./Data/ProjectData";
+import Separator from "../Separator/Separator";
+import ProjectCard from "./Project Cards/ProjectCard";
 import "./projects.scss";
-
 
 export default function Projects() {
   const data = ProjectData;
   return (
     <div className="projects" id="projects">
-      <label className="projectTitle" >Projects</label>
-      {data.map((project, index)=>{
-        return <ProjectCard project={project} key={index} />
+      <Separator />
+      <label className="projectTitle">Projects</label>
+      {data.map((project, index) => {
+        return <ProjectCard project={project} key={index} />;
       })}
     </div>
   );
---
+++ src/components/Separator/Separator.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './separator.scss'
+
+export default function Seperator(){
+    return (
+        <div className="separator"></div>
+    )
+}
