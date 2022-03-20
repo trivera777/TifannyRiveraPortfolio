abb2a2316b6020de4ef3f74b321ee98fa13d3e5e
Tifanny Rivera <trivera51580@gmail.com>
2022-03-20 11:50:54 -0600

    working skills icons

--- src/components/Skills/Data/SkillsData.jsx
+++ src/components/Skills/Data/SkillsData.jsx
@@ -6,15 +6,27 @@ export const SkillsData =[
         list: [
             {
                 name:'ReactJS',
-                icon: <i className="devicon-react-original colored"></i>,
+                icon: <i className="devicon-react-original colored"></i>
             },
             {
-                name:'ReactJS',
-                icon: <i className="devicon-react-original colored"></i>,
+                name:'JavaScript',
+                icon: <i className="devicon-javascript-plain colored"></i>                ,
             },
             {
-                name:'ReactJS',
-                icon: <i className="devicon-react-original colored"></i>,
+                name:'HTML',
+                icon: <i className="devicon-html5-plain colored"></i>                ,
+            },
+            {
+                name:'CSS',
+                icon: <i className="devicon-css3-plain colored"></i>                ,
+            },
+            {
+                name:'Heroku',
+                icon: <i className="devicon-heroku-plain colored"></i>                ,
+            },
+            {
+                name:'GitHub',
+                icon: <i className="devicon-heroku-plain colored"></i>                ,
             }
         ]
     }
