faf37e3b41b448053e45d1ab1680d13d28fb017e
Tifanny Rivera <trivera51580@gmail.com>
2022-03-20 18:06:18 -0600

    contact component finished. now working on footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export default function Contact(){
                     <p>Get in touch.</p>
                     <Social />
                 </div>
-                <div className="download">
+                <div className="resume">
                     <a download href="https://docs.google.com/document/d/1gppOgqWC_P0gN5zJT8u9jNp083smO5lA/edit?usp=sharing&ouid=109568298851160236684&rtpof=true&sd=true">
                     <i className="fi fi-rr-document"></i>
                         view resume
