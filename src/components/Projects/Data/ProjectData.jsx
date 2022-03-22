86272e472ed2e5fc93521b310c9cbad75dcb8378
Tifanny Rivera <trivera51580@gmail.com>
2022-03-22 13:42:38 -0600

    working project info section

--- src/components/Projects/Data/ProjectData.jsx
+++ src/components/Projects/Data/ProjectData.jsx
@@ -9,48 +9,48 @@ export const ProjectData = [
     },
     {
       id: "2",
-      title: "project",
-      about: "",
+      title: "Social Media App",
+      about: "This is a mock social media application where user can login with their username. User is able to create posts with text or with an image. When the user is signed in they are able to delete their own posts. User has the option to logout.",
       tags:["ReactJS"],
       demo:'https://tr-socialmediaapp.herokuapp.com/',
       github:'https://github.com/trivera777/SocialMediaApp',
-      image:'img'
+      image:'./assets/newpost.png'
     },
     {
       id: "3",
       title: "National Parks Day Trip Planner",
-      about: "This National Parks Day Trip Planner helps you plan your next trip to any state in the United States.",
+      about: "This National Parks Day Trip Planner helps you plan your next trip to any state in the United States. about",
       tags:["HTML", "CSS", "JavaScript", 'JQuery', 'Bulma'],
       demo:'https://trivera777.github.io/National-Parks-Day-Trip-Planner/',
       github:'https://github.com/trivera777/National-Parks-Day-Trip-Planner',
       image:'./assets/np.png'
     },
-    {
-      id: "4",
-      title: "Tech Blog",
-      about: "this project is about",
-      tags:["JavaScript", 'Node.js', 'MongoDB', 'Handlebars', 'MVC', 'MySQL'],
-      demo:'https://tr-techblog.herokuapp.com/',
-      github:'https://github.com/trivera777/TechBlog',
-      image:'./assets/techblog.png'
-    },
-    {
-      id: "5",
-      title: "project",
-      about: "this project is about",
-      tags:["ReactJS"],
-      demo:'demo',
-      github:'github',
-      image:'img'
-    },
-    {
-      id: "6",
-      title: "project",
-      about: "this project is about",
-      tags:["ReactJS"],
-      demo:'demo',
-      github:'github',
-      image:'img'
-    }
+    // {
+    //   id: "4",
+    //   title: "Tech Blog",
+    //   about: "this project is about",
+    //   tags:["JavaScript", 'Node.js', 'MongoDB', 'Handlebars', 'MVC', 'MySQL'],
+    //   demo:'https://tr-techblog.herokuapp.com/',
+    //   github:'https://github.com/trivera777/TechBlog',
+    //   image:'./assets/techblog.png'
+    // },
+    // {
+    //   id: "5",
+    //   title: "project",
+    //   about: "this project is about",
+    //   tags:["ReactJS"],
+    //   demo:'demo',
+    //   github:'github',
+    //   image:'img'
+    // },
+    // {
+    //   id: "6",
+    //   title: "project",
+    //   about: "this project is about",
+    //   tags:["ReactJS"],
+    //   demo:'demo',
+    //   github:'github',
+    //   image:'img'
+    // }
   ];
   
