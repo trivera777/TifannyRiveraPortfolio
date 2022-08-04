aaf2936ec40e6592acaac10eae9c71473fc59d02
Tifanny Rivera <trivera51580@gmail.com>
2022-08-04 13:21:08 -0600

    working heroku deploy

--- src/components/Projects/Data/ProjectData.jsx
+++ src/components/Projects/Data/ProjectData.jsx
@@ -52,5 +52,23 @@ export const ProjectData = [
     //   github:'github',
     //   image:'img'
     // }
+    // {
+    //   id: "6",
+    //   title: "project",
+    //   about: "this project is about",
+    //   tags:["ReactJS"],
+    //   demo:'demo',
+    //   github:'github',
+    //   image:'img'
+    // }
+    // {
+    //   id: "6",
+    //   title: "project",
+    //   about: "this project is about",
+    //   tags:["ReactJS"],
+    //   demo:'demo',
+    //   github:'github',
+    //   image:'img'
+    // }
   ];
   
