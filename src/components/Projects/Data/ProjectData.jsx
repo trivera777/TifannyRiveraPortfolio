ea10baa68b55fd60e3ed09b0819aeacb3a2bfd32
Tifanny Rivera <trivera51580@gmail.com>
2022-11-06 14:54:27 -0700

    working font transformation on component titles

--- src/components/Projects/Data/ProjectData.jsx
+++ src/components/Projects/Data/ProjectData.jsx
@@ -3,13 +3,22 @@ export const ProjectData = [
       id: "1",
       title: "Absolute Notary",
       about: "I had the opportunity to work with my friend and business owner, Jasmin Ponce. Although her clothing site was made with Wix.com, it was an awesome learning experience for my first freelance job.",
-      tags: ["HTML", "SASS", "ReactJS", "React Bootstrap"],
+      tags: ["HTML", "CSS", "ReactJS", "React Bootstrap"],
       demo:'https://www.merakioasis.store/',
       github:'https://github.com/trivera777/AbsoluteNotaryLLC',
-      image:'./assets/meraki.png'
+      image:'./assets/'
     },
     {
       id: "2",
+      title: "Portfolio",
+      about: "I had the opportunity to work with my friend and business owner, Jasmin Ponce. Although her clothing site was made with Wix.com, it was an awesome learning experience for my first freelance job.",
+      tags: ["HTML", "SASS", "ReactJS", "React Bootstrap"],
+      demo:'https://tifannyrivera.herokuapp.com/',
+      github:'https://github.com/trivera777/TifannyRiveraPortfolio',
+      image:'./assets/trportfolio.png'
+    },
+    {
+      id: "3",
       title: "Meraki Oasis",
       about: "I had the opportunity to work with my friend and business owner, Jasmin Ponce. Although her clothing site was made with Wix.com, it was an awesome learning experience for my first freelance job.",
       tags:["Wix.com"],
@@ -17,7 +26,7 @@ export const ProjectData = [
       image:'./assets/meraki.png'
     },
     {
-      id: "3",
+      id: "4",
       title: "Social Buzz",
       about: "This is a mock social media application where user can login and create a username. When the user is signed in they are able to delete their own posts and will have the option to logout.",
       tags:["ReactJS", "React Bootstrap"],
@@ -26,7 +35,7 @@ export const ProjectData = [
       image:'./assets/socialbuzz.png'
     },
     {
-      id: "4",
+      id: "5",
       title: "National Parks Day Trip Planner",
       about: "This National Parks Day Trip Planner helps you plan your next trip to any state in the United States.",
       tags:["HTML", "CSS", "JavaScript", 'JQuery', 'Bulma'],
@@ -35,7 +44,7 @@ export const ProjectData = [
       image:'./assets/np.png'
     },
     {
-      id: "5",
+      id: "6",
       title: "Tech Blog",
       about: "This app is a tech blog that allows users to create a profile to be able to create, edit and delete their own blog posts. User is also able to comment on other users posts.",
       tags:["JavaScript", 'Node.js', 'Express.js', 'Handlebars.js', 'MVC', 'MySQL', 'Sequelize'],
@@ -44,7 +53,7 @@ export const ProjectData = [
       image:'./assets/techblog.png'
     },
     {
-      id: "6",
+      id: "7",
       title: "To Do list",
       about: "This is a simple To-Do List. List your items and once they are completed you can click the item once to cross it off and when you are ready you can delete the item off the list.",
       tags:["HTML", "CSS", "JavaScript"],
